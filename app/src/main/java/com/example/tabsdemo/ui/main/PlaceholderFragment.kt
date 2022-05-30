42bae722dcb91dfe1672462bb8fda64ae545cea8
Никита Трощенко <nitrix38r@gmail.com>
2022-05-30 20:04:15 +0800

    commit

--- app/src/main/java/com/example/tabsdemo/ui/main/PlaceholderFragment.kt
+++ app/src/main/java/com/example/tabsdemo/ui/main/PlaceholderFragment.kt
@@ -1,15 +1,21 @@
 package com.example.tabsdemo.ui.main
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.tabsdemo.R
 import com.example.tabsdemo.databinding.FragmentMainBinding
+import android.widget.ArrayAdapter
+
+
+
 
 // TODO: создать фрагмент со сведениями о погоде
 class PlaceholderFragment : Fragment() {
@@ -21,26 +27,37 @@ class PlaceholderFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        pageViewModel = ViewModelProvider(this).get(PageViewModel::class.java).apply {
-            setIndex(arguments?.getInt(ARG_SECTION_NUMBER) ?: 1)
-        }
-    }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        pageViewModel = ViewModelProvider(this).get(PageViewModel::class.java).apply {
+//            setIndex(arguments?.getInt(ARG_SECTION_STRINGS) ?: 1)
+//        }
+//    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+        if(arguments!=null) {
+            val ids = arrayListOf<Int>(R.array.action, R.array.drama, R.array.comedy)
+            val bundle = arguments
 
-        _binding = FragmentMainBinding.inflate(inflater, container, false)
-        val root = binding.root
+            val position = bundle!!.getInt("number")
+            val listView = view.findViewById<ListView>(R.id.movies)
+            var adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, resources.getStringArray(ids[position]))
+            listView.adapter = adapter
+        }
+        return view
 
-        val textView: TextView = binding.sectionLabel
-        pageViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+//        _binding = FragmentMainBinding.inflate(inflater, container, false)
+//        val root = binding.root
+//
+//        val textView: TextView = binding.sectionLabel
+//        pageViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
+//        return root
     }
 
     companion object {
@@ -48,17 +65,17 @@ class PlaceholderFragment : Fragment() {
          * The fragment argument representing the section number for this
          * fragment.
          */
-        private const val ARG_SECTION_NUMBER = "section_number"
+        private const val ARG_SECTION_NUMBER = "number"
 
         /**
          * Returns a new instance of this fragment for the given section
          * number.
          */
         @JvmStatic
-        fun newInstance(sectionNumber: Int): PlaceholderFragment {
+        fun newInstance(number: Int): PlaceholderFragment {
             return PlaceholderFragment().apply {
                 arguments = Bundle().apply {
-                    putInt(ARG_SECTION_NUMBER, sectionNumber)
+                    putInt(ARG_SECTION_NUMBER, number)
                 }
             }
         }
--- app/src/main/java/com/example/tabsdemo/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/tabsdemo/ui/main/SectionsPagerAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.tabsdemo.ui.main
 
 import android.content.Context
+import android.content.res.Resources
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
@@ -11,19 +12,21 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
     FragmentPagerAdapter(fm) {
 
     override fun getItem(position: Int): Fragment {
+        //val ids = arrayListOf<Int>(R.array.action, R.array.drama, R.array.comedy)
+        //var array = context.resources.getStringArray(ids[position])
         // TODO: создать фрагмент со сведениями о погоде
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1)
+        return PlaceholderFragment.newInstance(position)
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
         // TODO: задать заголовок - название города
-        return context.resources.getStringArray(R.array.cities)[position]
+        return context.resources.getStringArray(R.array.films)[position]
     }
 
     override fun getCount(): Int {
         // TODO: возвращать число доступных городов
-        return context.resources.getStringArray(R.array.cities).size
+        return context.resources.getStringArray(R.array.films).size
     }
 }
